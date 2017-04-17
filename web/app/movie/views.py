926df2e1bd484a2eb42b7bc1ec2e688ba44f9bed
Zhao Peng-ya <zpy1101936864@163.com>
2017-04-17 17:12:53 +0800

    调用豆瓣api搜索

--- web/app/movie/views.py
+++ web/app/movie/views.py
@@ -1,5 +1,6 @@
 from . import movie
 from flask import render_template, request
+import requests, json
 
 @movie.route('/tagList')
 def tagList():
@@ -7,3 +8,10 @@ def tagList():
         taglt = eval(f.read())
         f.close()
     return render_template('movie/tag.html', taglt=taglt)
+
+@movie.route('/search/<keyword>')
+def search(keyword):
+    start_url=r'https://api.douban.com/v2/movie/search?q='
+    url=start_url+keyword
+    r = requests.get(url=url)
+    return render_template('movie/searchResult.html', movieInfo=r.json())
